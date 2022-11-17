90a9cbb1356b0d7153d3fa276181ad5fc1cea8f6
Arjun5722 <113657142+Arjun5722@users.noreply.github.com>
2022-11-17 23:41:21 +0530

    Add files via upload

---
+++ PROJECT DEVELOPMENT PHASE/Sprint 3/header.php
@@ -0,0 +1,14 @@
+<div class="brand clearfix">
+	<a href="dashboard.php" style="font-size: 30px; padding-top:1%; color:#fff">PLASMA BANK & DONOR MANAGEMENT SYSTEM </a>  
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="https://lo5.s3.jp-tok.cloud-object-storage.appdomain.cloud/images.jpg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">Change Password</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
